b590ba104d2bf5ca86b8defce712e3ddf9d80cbc
sathana <sathana.rd2023cse@sece.ac.in>
2025-08-02 00:02:12 +0530

    backend update2 done

--- Symptom-Advicer/src/main/java/com/example/Symptom_Advicer/config/SpringController.java
+++ Symptom-Advicer/src/main/java/com/example/Symptom_Advicer/config/SpringController.java
@@ -87,7 +87,7 @@ public class SpringController {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("http://localhost:3000", "http://localhost:5173"));
+        config.setAllowedOriginPatterns(List.of("http://localhost:3000", "http://localhost:5173","https://symptom-advicer-2.onrender.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
